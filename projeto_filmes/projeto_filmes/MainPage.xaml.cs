86140ab036fc857ad23ef84925471f71ee288a6a
Usuario <Usuario@CPD02-M07>
2024-03-18 11:39:55 -0300

    Adicionar arquivos de projeto.

---
+++ projeto_filmes/projeto_filmes/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using projeto_filmes.categoria;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace projeto_filmes
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new MainPage());
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("ops", ex.Message, "ok");
+            }
+        }
+    }
+}
